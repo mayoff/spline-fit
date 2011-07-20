9c017ae05fd0fedb9b35b694c19feebf0fe3ae9f
Rob Mayoff <git@rob.dqd.com>
2011-07-20 01:38:06 -0500

    some cleanups; dynamic fitting as you drag the mouse

--- main.js
+++ main.js
@@ -34,133 +34,144 @@ Vector.prototype.dot = function (rhs) {
 };
 
 function UnconstrainedFitter() {
-    // vector (x[i], y[i]) is a data vector the curve must try to pass through.
-    this.x = [];
-    this.y = [];
+    // I will try to pass the curve through each (xs[i], ys[i]) in order.
+    this.xs = [];
+    this.ys = [];
 
-    // d[i] is the sum f the straight-line distances between consecutive data vectors up to and including vector i.
-    this.d = [];
+    // this.ps[i]/this.pmax is the parameter value at which I try to fit the curve to (xs[i], ys[i]).
+    this.ps = [];
+    this.pmax = 0.0;
 
-    // dmax is sum of the straight-line distances between consecutive data vectors.
-    this.dmax = 0.0;
+    // (cxs[i], cys[i]) is a control point of the fitted Bezier cubic.
+    this.cxs = [];
+    this.cys = [];
 
-    // vector (cx[i], cy[i]) is a control vector of the fitted Bezier cubic.
-    this.cx = [];
-    this.cy = [];
+    // (cxs1[i], cys1[i]) is a control point of the first derivative of the fitted Bezier cubic.  The
+    // first derivative of a Bezier cubic is a Bezier quadratic.
+    this.cxs1 = [];
+    this.cys1 = [];
 
-    // vector (cx1[i], cy1[i]) is a control vector of the first derivative of the fitted Bezier cubic.
-    this.cx1 = [];
-    this.cy1 = [];
-
-    // vector (cx2[i], cy2[i]) is a control vector of the second derivative of the fitted Bezier cubic.
-    this.cx2 = [];
-    this.cy2 = [];
+    // (cxs2[i], cys2[i]) is a control point of the second derivative of the fitted Bezier cubic.  The
+    // second derivative of a Bezier cubic is a line.
+    this.cxs2 = [];
+    this.cys2 = [];
 }
 
+UnconstrainedFitter.prototype.mdump = function () {
+    console.log("xs=List" + JSON.stringify(this.xs) + ";\n" +
+        "ys=List" + JSON.stringify(this.ys) + ";\n" +
+        "cxs=List" + JSON.stringify(this.cxs) + ";\n" +
+        "cys=List" + JSON.stringify(this.cys) + ";\n");
+};
+
 UnconstrainedFitter.prototype.push = function (x, y) {
-    var dx, dy, d;
-    if (this.x.length > 0) {
-        dx = x - this.x.last();
-        dy = y - this.y.last();
-        d = Math.sqrt(dx*dx + dy*dy);
-        this.d.push(this.d.last() + d);
-    } else {
-        this.d.push(0);
-    }
-    this.dmax = this.d.last();
-    this.x.push(x);
-    this.y.push(y);
+    this.xs.push(x);
+    this.ys.push(y);
 };
 
 UnconstrainedFitter.prototype.fit = function () {
-    var x = this.x, y = this.y, l = x.length, u, i, d = this.d, dmax = this.dmax, cx, cy, cx1, cy1;
-
-    if (l < 4) {
+    if (this.xs.length < 4) {
         // XXX Need a different method to fit the curve.
         return;
     }
 
-    u = new Array(l);
-    for (i = 0; i < l; ++i)
-        { u[i] = d[i] / dmax; }
-
-    cx = [ x[0], 0.0, 0.0, x.last() ];
-    cy = [ y[0], 0.0, 0.0, y.last() ];
-
-    this.fitAxis(x, u, cx);
-    this.fitAxis(y, u, cy);
-
-    this.cx = cx;
-    this.cy = cy;
-
-    cx1 = this.cx1 = [ cx[1]-cx[0], cx[2]-cx[1], cx[3]-cx[2] ];
-    cy1 = this.cy1 = [ cy[1]-cy[0], cy[2]-cy[1], cy[3]-cy[2] ];
-
-    this.cx2 = [ cx1[1] - cx1[0], cx1[2] - cx1[1] ];
-    this.cy2 = [ cy1[1] - cy1[0], cy1[2] - cy1[1] ];
+    this.pickInitialPs();
+    this.cxs = cxs = this.fitAxis(this.xs, this.ps, this.pmax);
+    this.cys = cys = this.fitAxis(this.ys, this.ps, this.pmax);
+    this.cxs1 = this.derivativeControlPointsFor(this.cxs);
+    this.cys1 = this.derivativeControlPointsFor(this.cys);
+    this.cxs2 = this.derivativeControlPointsFor(this.cxs1);
+    this.cys2 = this.derivativeControlPointsFor(this.cys1);
 };
 
-UnconstrainedFitter.prototype.fitAxis = function (x, u, cx) {
-    var i, l = x.length, m00 = 0.0, m01 = 0.0, m10 = 0.0, m11 = 0.0, y0 = 0.0, y1 = 0.0, B0, B1, B2, B3, xi, ui, uim, cx0 = cx[0], cx3 = cx[3];
+UnconstrainedFitter.prototype.pickInitialPs = function () {
+    var xs = this.xs, ys = this.ys, l = xs.length, i, ps = new Array(l), x, y, lx = xs[0], ly = ys[0], dx, dy;
+
+    ps[0] = 0.0;
+    for (i = 1; i < l; ++i) {
+        x = xs[i]; y = ys[i];
+        dx = x - lx; dy = y - ly;
+        ps[i] = ps[i-1] + Math.sqrt(dx*dx + dy*dy);
+        lx = x; ly = y;
+    }
+    this.ps = ps;
+    this.pmax = ps.last();
+};
+
+UnconstrainedFitter.prototype.derivativeControlPointsFor = function (cp) {
+    var n = cp.length - 1, r = new Array(n), i;
+    for (i = 0; i < n; ++i) {
+        r[i] = n * (cp[i+1] - cp[i]);
+    }
+    return r;
+}
+
+UnconstrainedFitter.prototype.fitAxis = function (xs, ps, pmax) {
+    var i, l = xs.length, x, t, tm, x0 = xs[0], xe = xs.last(),
+        m00 = 0.0, m01 = 0.0, m10 = 0.0, m11 = 0.0, y0 = 0.0, y1 = 0.0, B0, B1, B2, B3;
 
     for (i = 0; i < l; ++i) {
-        xi = x[i];
-        ui = u[i];
-        uim = 1 - u[i];
+        x = xs[i];
+        t = ps[i] / pmax;
+        tm = 1 - t;
 
-        B0 = uim*uim*uim;
-        B1 = 3*ui*uim*uim;
-        B2 = 3*ui*ui*uim;
-        B3 = ui*ui*ui;
+        B0 = tm*tm*tm;
+        B1 = 3*t*tm*tm;
+        B2 = 3*t*t*tm;
+        B3 = t*t*t;
 
         m00 += B1*B1;
         m01 += B1*B2;
         m10 += B1*B2;
         m11 += B2*B2;
 
-        y0 += B1 * (xi - cx0 * B0 - cx3 * B3);
-        y1 += B2 * (xi - cx0 * B0 - cx3 * B3);
+        y0 += B1 * (x - x0 * B0 - xe * B3);
+        y1 += B2 * (x - x0 * B0 - xe * B3);
     }
 
-    cx[1] = (m11*y0 - m01*y1)/(m00*m11 - m01*m10);
-    cx[2] = (m10*y0 - m00*y1)/(m01*m10 - m00*m11);
+    return [ x0, (m11*y0 - m01*y1)/(m00*m11 - m01*m10), (m10*y0 - m00*y1)/(m01*m10 - m00*m11), xe ];
 };
 
-UnconstrainedFitter.prototype.vectorAt = function (t) {
-    var tm = 1 - t, cx = this.cx, cy = this.cy;
+UnconstrainedFitter.prototype.vectorAt = function (p) {
+    var t = p / this.pmax, tm = 1 - t, cxs = this.cxs, cys = this.cys;
     return new Vector(
-        tm*tm*tm*cx[0] + 3*tm*tm*t*cx[1] + 3*tm*t*t*cx[2] + t*t*t*cx[3],
-        tm*tm*tm*cy[0] + 3*tm*tm*t*cy[1] + 3*tm*t*t*cy[2] + t*t*t*cy[3]);
+        tm*tm*tm*cxs[0] + 3*tm*tm*t*cxs[1] + 3*tm*t*t*cxs[2] + t*t*t*cxs[3],
+        tm*tm*tm*cys[0] + 3*tm*tm*t*cys[1] + 3*tm*t*t*cys[2] + t*t*t*cys[3]);
 };
 
-UnconstrainedFitter.prototype.derivativeAt = function (t) {
-    var tm = 1 - t, cx = this.cx1, cy = this.cy1;
+UnconstrainedFitter.prototype.derivativeAt = function (p) {
+    var t = p / this.pmax, tm = 1 - t, cxs = this.cxs1, cys = this.cys1;
     return new Vector(
-        tm*tm*cx[0] + 2*tm*t*cx[1] + t*t*cx[2],
-        tm*tm*cy[0] + 2*tm*t*cy[1] + t*t*cy[2]);
+        tm*tm*cxs[0] + 2*tm*t*cxs[1] + t*t*cxs[2],
+        tm*tm*cys[0] + 2*tm*t*cys[1] + t*t*cys[2]);
 };
 
-
-UnconstrainedFitter.prototype.secondDerivativeAt = function (t) {
-    var tm = 1 - t, cx = this.cx2, cy = this.cy2;
+UnconstrainedFitter.prototype.secondDerivativeAt = function (p) {
+    var t = p / this.pmax, tm = 1 - t, cxs = this.cxs2, cys = this.cys2;
     return new Vector(
-        tm*cx[0] + t*cx[1],
-        tm*cy[0] + t*cy[1]);
+        tm*cxs[0] + t*cxs[1],
+        tm*cys[0] + t*cys[1]);
 };
 
 UnconstrainedFitter.prototype.improveDataParameters = function () {
-    var i, l = this.x.length;
-    for (i = 0; i < l; ++i)
-        { this.d[i] = this.dmax * this.improveDataParameter(this.x[i], this.y[i], this.d[i] / this.dmax); }
+    var i, l = this.xs.length;
+    for (i = 0; i < l; ++i) {
+        this.improveDataParameter(i);
+    }
 };
 
-UnconstrainedFitter.prototype.improveDataParameter = function (x, y, t) {
-    var Q = this.vectorAt(t),
+UnconstrainedFitter.prototype.improveDataParameter = function (i) {
+    var x = this.xs[i], y = this.ys[i], t = this.ps[i] / this.pmax,
+        Q = this.vectorAt(t),
         D = Q.minus(new Vector(x, y)),
         Qp = this.derivativeAt(t),
-        Qpp = this.secondDerivativeAt(t);
+        Qpp = this.secondDerivativeAt(t),
+        t1;
 
-    return t - D.dot(Qp) / (D.dot(Qpp) + Qp.dot(Qp));
+    t1 = t - D.dot(Qp) / (D.dot(Qpp) + Qp.dot(Qp));
+    if (t1 >= 0 && t1 <= 1) {
+        this.ps[i] = t1 * this.pmax;
+    }
 };
 
 canvasController = {
@@ -169,6 +180,7 @@ canvasController = {
     gc: null,
     vectors: null,
     fitter: null,
+    tracking: false,
 
     init: function () {
         bindMethods(this);
@@ -184,105 +196,101 @@ canvasController = {
     },
 
     mouseDown: function (event) {
+        if (this.tracking) {
+            return;
+        }
+
         document.addEventListener('mousemove', this.mouseDragged);
         document.addEventListener('mouseup', this.mouseUp);
-
-        this.gc.strokeStyle = 'black';
-        this.gc.lineWidth = 1;
-        this.gc.lineCap = 'round';
-        this.gc.fillStyle = 'red';
-
+        this.tracking = true;
         this.fitter = new UnconstrainedFitter();
-        this.startSegment(this.mouseVectorForEvent(event));
+        this.addPoint(this.mouseVectorForEvent(event));
     }.bindLater(),
 
     mouseDragged: function (event) {
-        var p = this.mouseVectorForEvent(event),
-            dx = p.x - this.fitter.x.last(),
-            dy = p.y - this.fitter.y.last();
-
-        if (dx*dx + dy*dy < 1000)
-            return;
-
-        this.endSegment(p);
-        this.startSegment(p);
+        this.addPoint(this.mouseVectorForEvent(event));
     }.bindLater(),
 
     mouseUp: function (event) {
         document.removeEventListener('mousemove', this.mouseDragged);
         document.removeEventListener('mouseup', this.mouseUp);
+        this.tracking = false;
+    }.bindLater(),
 
-        var fitter = this.fitter, x, y, d, dmax, cx, cy, gc = this.gc, i, l, p;
+    addPoint: function (point) {
+        if (!this.pointIsWorthAdding(point))
+            return;
+        this.fitter.push(point.x, point.y);
+        this.gc.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.gc.lineWidth = 1;
+        this.drawPolyline('gray', 'black', this.fitter.xs, this.fitter.ys);
 
-        fitter.fit();
-        x = fitter.x;
-        y = fitter.y;
-        d = fitter.d;
-        dmax = fitter.dmax;
-        cx = fitter.cx;
-        cy = fitter.cy;
-        l = x.length;
+        this.fitter.fit();
+        this.fitter.improveDataParameters();
+        this.fitter.fit();
 
-        gc.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawPolyline('gray', 'gray', this.fitter.cxs, this.fitter.cys);
+        this.drawConnectors();
+        this.gc.lineWidth = 2;
+        this.drawCubic('blue', this.fitter.cxs, this.fitter.cys);
+    },
 
-        gc.strokeStyle = 'black';
-        gc.lineWidth = 1;
+    pointIsWorthAdding: function (point) {
+        if (this.fitter.xs.length === 0) {
+            return true;
+        }
+        var dx = point.x - this.fitter.xs.last(), dy = point.y - this.fitter.ys.last();
+        return dx*dx + dy*dy >= 4;
+    },
+
+    drawPoint: function (x, y) {
+        this.gc.fillRect(x - 1.5, y - 1.5, 3, 3);
+    },
+
+    drawPolyline: function (lineColor, pointColor, xs, ys) {
+        var gc = this.gc, i, l = xs.length;
+        if (lineColor !== null) {
+                gc.strokeStyle = lineColor;
+            gc.beginPath();
+            gc.moveTo(xs[0], ys[0]);
+            for (i = 1; i < l; ++i) {
+                gc.lineTo(xs[i], ys[i]);
+            }
+            gc.stroke();
+        }
+
+        if (pointColor != null) {
+            gc.fillStyle = pointColor;
+            for (i = 0; i < l; ++i) {
+                this.drawPoint(xs[i], ys[i]);
+            }
+        }
+    },
+
+    drawCubic: function (color, cxs, cys) {
+        var gc = this.gc;
+        gc.strokeStyle = color;
         gc.beginPath();
-        gc.moveTo(x[0], y[0]);
-        for (i = 1; i < l; ++i)
-            { gc.lineTo(x[i], y[i]); }
+        gc.moveTo(cxs[0], cys[0]);
+        gc.bezierCurveTo(cxs[1], cys[1], cxs[2], cys[2], cxs[3], cys[3]);
         gc.stroke();
+    },
 
-        gc.strokeStyle = 'blue';
-        gc.beginPath();
-        gc.moveTo(cx[0], cy[0]);
-        gc.bezierCurveTo(cx[1], cy[1], cx[2], cy[2], cx[3], cy[3]);
-        gc.stroke();
-
-        gc.strokeStyle = 'grey';
-        gc.beginPath();
-        gc.moveTo(cx[0], cy[0]);
-        for (i = 1; i < 4; ++i)
-            { gc.lineTo(cx[i], cy[i]); }
-        gc.stroke();
+    drawConnectors: function () {
+        var gc = this.gc, fitter = this.fitter, xs = fitter.xs, ys = fitter.ys, ps = fitter.ps,
+            i, l = xs.length, v;
 
         gc.fillStyle = 'red';
         gc.strokeStyle = 'red';
+        gc.lineWidth = 1;
         for (i = 0; i < l; ++i) {
-            gc.fillRect(x[i] - 1.5, y[i] - 1.5, 3, 3);
-            p = fitter.vectorAt(d[i] / dmax);
-            gc.fillRect(p.x - 1.5, p.y - 1.5, 3, 3);
+            v = fitter.vectorAt(ps[i]);
+            this.drawPoint(v.x, v.y);
             gc.beginPath();
-            gc.moveTo(x[i], y[i]);
-            gc.lineTo(p.x, p.y);
+            gc.moveTo(v.x, v.y);
+            gc.lineTo(xs[i], ys[i]);
             gc.stroke();
         }
-
-        fitter.improveDataParameters();
-        gc.fillStyle = 'green';
-        gc.strokeStyle = 'green';
-        for (i = 0; i < l; ++i) {
-            p = fitter.vectorAt(d[i] / dmax);
-            gc.fillRect(p.x - 1.5, p.y - 1.5, 3, 3);
-            gc.beginPath();
-            gc.moveTo(x[i], y[i]);
-            gc.lineTo(p.x, p.y);
-            gc.stroke();
-        }
-    }.bindLater(),
-
-    startSegment: function (vector) {
-        var gc = this.gc;
-        gc.fillRect(vector.x - 1.5, vector.y - 1.5, 3, 3);
-        gc.beginPath();
-        gc.moveTo(vector.x, vector.y);
-        this.fitter.push(vector.x, vector.y);
-    },
-
-    endSegment: function (vector) {
-        var gc = this.gc;
-        gc.lineTo(vector.x, vector.y);
-        gc.stroke();
     }
 
 };
