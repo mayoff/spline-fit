f5d4ce1a4b436e288799af6b7b8a24bc4f9c9d81
Rob Mayoff <git@rob.dqd.com>
2011-07-26 18:39:09 -0500

    add Function.trace for debugging

--- misc.js
+++ misc.js
@@ -6,3 +6,20 @@ Object.defineProperty(Array.prototype, 'last', {
     set: function (value) { return this[this.length] = value; }
 });
 
+Function.trace = function (object, method) {
+    var wrapper = function () {
+        console.log("calling " + method, this, arguments);
+        var r = wrapper.original.apply(this, arguments);
+        console.log("returning from " + method, r);
+        return r;
+    };
+    wrapper.original = object[method];
+    object[method] = wrapper;
+    return wrapper;
+};
+
+Function.prototype.untrace = function (object, method) {
+    object[method] = object[method].original;
+    return object[method];
+};
+
